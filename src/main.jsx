086b11d1df04e7018a0d064d2ace04c1a2235e11
JOAO VICTOR VIEIRA DE OLIVEIRA <joao.oliveira1508>
2026-03-10 10:56:00 -0300

    Aula 10/03

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 
 import './estilizacao/reset.css';
-import './estilizacao/global.css';
+import './estilizacao/globals.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
