0319c0f445f72b908350f5428beb966a3c5e9b45
jvvieiraoliveira2008-dev <jvvieiraoliveira2008@gmail.com>
2026-03-23 10:48:44 -0300

    Aula 23/03

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import Home from './pages/home';
 import Exemplo01 from './pages/exemplos/ex-01';
 import Exemplo02 from './pages/exemplos/ex-02';
 
+
+import Atividades01 from './pages/atividades/atv-01';
+
+
 function App() {
 
   return (
@@ -11,8 +15,11 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/exemplo/1" element={<Exemplo01 />} />
       <Route path="/exemplo/2" element={<Exemplo02 />} />
+
+       <Route path="/atividades01" element={<Atividades01 />} />
+
     </Routes>
   )
 }
 
-export default App;
+export default App; 
---
+++ src/pages/atividades/atv-01/index.jsx
@@ -0,0 +1,10 @@
+function Atividades01() {
+     return (
+          <>
+            <h1>Atividades01</h1>  
+            <p>Componente basico para renderizar tla com react</p>
+         </>
+   )
+}
+
+export default Atividades01;
